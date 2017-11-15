67a65e6e8ee95bf84db1803a7d11a6a14b23191c
cnguyenm <cnguyenm@purdue.edu>
2017-11-15 04:25:32 +0000

    Add app roomalloc * basic frame * basic url, webpage

---
+++ roomalloc/urls.py
@@ -0,0 +1,23 @@
+
+from django.conf.urls import url 
+
+from roomalloc.view import public, account
+
+# register app_name for template url
+app_name = 'roomalloc'
+
+# url patterns
+urlpatterns = [
+
+    # public 
+    url(r'^$'               , public.index  , name='index'),
+    url(r'^public/about/$'  , public.about  , name='about'),
+    url(r'^public/contact/$', public.contact, name='contact'),
+    
+    # account
+    url(r'^account/signup/$', account.signup, name='signup'),
+    url(r'^account/login/$' , account.login , name='login'),
+    url(r'^account/logout/$', account.logout, name='logout'),
+    
+]
+
---
+++ roomalloc/view/public.py
@@ -0,0 +1,21 @@
+"""
+public.py:
+Render the public page
+"""
+
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+from roomalloc.const import Template
+from roomalloc.const import TplConst as T
+
+def index(request):
+    return render(request, Template.PUBLIC_INDEX, {T.NBAR:"index"})
+    
+def about(request):
+    return render(request, Template.PUBLIC_ABOUT, {T.NBAR :"about"})
+    
+def contact(request):
+    return render(request, Template.PUBLIC_CONTACT, {T.NBAR:"contact"})
+    
